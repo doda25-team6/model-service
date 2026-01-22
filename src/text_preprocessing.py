5fe259ecc69baa3faeffd3f3f1f64f6829eeb3b8
cemgungor1 <ccemgungor@gmail.com>
2026-01-22 19:26:50 +0100

    fix: path to the model

--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -67,12 +67,12 @@ def _preprocess(messages):
     )
 
     preprocessed_data = preprocessor.fit_transform(messages['message'])
-    dump(preprocessor, 'output/preprocessor.joblib')
-    dump(preprocessed_data, 'output/preprocessed_data.joblib')
+    dump(preprocessor, '/app/output/preprocessor.joblib')
+    dump(preprocessed_data, '/app/output/preprocessed_data.joblib')
     return preprocessed_data
 
 def prepare(message):
-    preprocessor = load('output/preprocessor.joblib')
+    preprocessor = load('/app/output/preprocessor.joblib')
     return preprocessor.transform([message])
 
 
